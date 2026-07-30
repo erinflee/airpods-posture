a02a19702f385ca6b432e90a2626344f94010475
erinlee316 <127688889+erinflee@users.noreply.github.com>
2026-07-30 14:01:34 -0700

    add summarize_file to create dictionary full of statistics

--- inspect_data.py
+++ inspect_data.py
@@ -12,8 +12,8 @@ import sys
 from pathlib import Path
 
 import matplotlib.pyploy as plt
+from statistics import fmean, pstdev
 from config import BASELINE_PATH, DATA_DIR, CLASS_PREFIX_TO_ID
-
 
 def load_csv(csv_path):
     with open(csv_path, 'r', newline="", encoding="utf-8") as file:
@@ -21,12 +21,46 @@ def load_csv(csv_path):
         return list(reader)
 
 
-def label_from_filename(file):
-    filename = Path(file).stem # neutral_01.csv -> cuts .csv and returns neutral_01
-    prefix_string = filename.split("_")[0]
-    if prefix_string in CLASS_PREFIX_TO_ID.keys():
-        return prefix_string
+def label_from_filename(path):
+    filename = Path(path).stem # neutral_01.csv -> cuts .csv and returns neutral_01
+    prefix = filename.split("_")[0]
+    if prefix in CLASS_PREFIX_TO_ID.keys():
+        return prefix
     raise ValueError("invalid file name")
     
+
+def summarize_file(path, baseline):
+    rows = load_csv(path)
+    prefix = label_from_filename(path)
+    pitch = []
+    roll = []
+    delta_pitch = []
+    stats = {}    
+    for row in rows: 
+        pitch.append(row["pitch"])
+        roll.append(row["roll"])
+        pitch_delta.append(row["pitch"] - baseline["mean"]["pitch"])
+
+    pitch_mean = statistics.fmean(pitch)
+    roll_mean = statistics.fmean(roll)
+    delta_pitch_mean = statistics.fmean(delta_pitch)
+    
+    pitch_std = statistics.pstdev(pitch)
+    roll_std = statistics.pstdev(roll)
+    delta_pitch_std = statistics.pstdev(delta_pitch)
+
+    stats["sample_count"] = len(rows)
+    stats["label"] = prefix
+    stats["pitch_mean"] = pitch_mean
+    stats["roll_mean"] = roll_mean
+    stats["delta_pitch_mean"] = delta_pitch_mean
+    stats["pitch_std"] = pitch_std
+    stats["roll_std"] = roll_std
+    stats["delta_pitch_std"] = delta_pitch_std
+
+    return stats
+
+
+
 
 
