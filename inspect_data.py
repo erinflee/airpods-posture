b423ef70075031c069b6ab2038a13709db60a8e8
erinlee316 <127688889+erinflee@users.noreply.github.com>
2026-07-30 13:07:54 -0700

    add label_from_filename to take in file and output label as string

--- inspect_data.py
+++ inspect_data.py
@@ -12,10 +12,21 @@ import sys
 from pathlib import Path
 
 import matplotlib.pyploy as plt
-from config import BASELINE_PATH, DATA_DIR
+from config import BASELINE_PATH, DATA_DIR, CLASS_PREFIX_TO_ID
 
 
 def load_csv(csv_path):
     with open(csv_path, 'r', newline="", encoding="utf-8") as file:
         reader = csv.DictReader(file)
         return list(reader)
+
+
+def label_from_filename(file):
+    filename = Path(file).stem # neutral_01.csv -> cuts .csv and returns neutral_01
+    prefix_string = filename.split("_")[0]
+    if prefix_string in CLASS_PREFIX_TO_ID.keys():
+        return prefix_string
+    raise ValueError("invalid file name")
+    
+
+
