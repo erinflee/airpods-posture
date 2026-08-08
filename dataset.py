fe295e9eb5023b58df006cd9dae3888d5d8f3eed
erinlee316 <127688889+erinflee@users.noreply.github.com>
2026-08-08 07:55:02 -0700

    add class PostureWindowDataset() with three methods

--- dataset.py
+++ dataset.py
@@ -7,8 +7,22 @@ PyTorch Dataset
       (not random frames), return train_ds, val_ds, source filenames
 """
 
-
 import torch
 from torch.utils.data import Dataset
 from shared_features import load_labeled_windows
+
+
+class PostureWindowDataset(Dataset):
+  def __init__(self, x, y, ):
+    self.x = torch.tensor(x, dtype=torch.float32)
+    self.y = torch.tensor(y, dtype=torch.int64)
+
+  def __len__(self):
+    return len(self.x)
+
+  def __getitem__(self, i):
+    x = self.x[i]
+    y = self.y[i]
+    return x, y
+
 
