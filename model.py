11669276af0dc3c247f7f1f0ec798d9050cc4dbf
erinlee316 <127688889+erinflee@users.noreply.github.com>
2026-08-08 09:39:47 -0700

    add class PostureCNN() to extract features from a sequence and predict a class

--- model.py
+++ model.py
@@ -13,9 +13,9 @@ class PostureCNN(nn.Module):
     super().__init__()
     self.conv1 = nn.Conv1d(in_channels=6, out_channels=32, kernel_size=5, padding=2)
     self.conv2 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=5, padding=2)
-    self.pool = nn.MaxPool1s(kernel_size=2)
+    self.pool = nn.MaxPool1d(kernel_size=2)
     self.gap = nn.AdaptiveAvgPool1d(1) # global average pooling
-    self.relu = nn.Relu()
+    self.relu = nn.ReLU()
     self.fc = nn.Linear(in_features=64, out_features=num_classes)
 
   def forward(self, x):
