bb6261c59c696795e413dd5eb14bdb7f43bfb577
erinlee316 <127688889+erinflee@users.noreply.github.com>
2026-08-08 10:46:50 -0700

    add necessary file imports for file functionality

--- train.py
+++ train.py
@@ -3,3 +3,12 @@ Training loop
 
 """
 
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from config import BATCH_SIZE, EPOCHS, LEARNING_RATE, MODEL_PATH
+from dataset import build_datasets
+from model import PostureCNN
+
+
