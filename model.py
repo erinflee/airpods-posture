09d4ca37ce3685d70530ecf157a447e46ff3a4a2
erinlee316 <127688889+erinflee@users.noreply.github.com>
2026-08-08 09:38:58 -0700

    add class PostureCNN() to extract features from a sequence and predict a class

--- model.py
+++ model.py
@@ -1,12 +1,33 @@
 """
-1D-CNN for posture windows (TODO: implement).
+1D-CNN for posture windows 
 
-  - PostureCNN(nn.Module):
-      __init__(num_classes=3): Conv1d stack + Linear head
-      forward(x): (batch, features, time) -> logits (batch, num_classes)
 """
 
 import torch
 import torch.nn as nn
 from config import NUM_CLASSES
 
+
+class PostureCNN(nn.Module):
+  def __init__(self, num_classes=NUM_CLASSES):
+    super().__init__()
+    self.conv1 = nn.Conv1d(in_channels=6, out_channels=32, kernel_size=5, padding=2)
+    self.conv2 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=5, padding=2)
+    self.pool = nn.MaxPool1s(kernel_size=2)
+    self.gap = nn.AdaptiveAvgPool1d(1) # global average pooling
+    self.relu = nn.Relu()
+    self.fc = nn.Linear(in_features=64, out_features=num_classes)
+
+  def forward(self, x):
+    out1 = self.conv1(x)
+    relu1 = self.relu(out1)
+    pool1 = self.pool(relu1)
+
+    out2 = self.conv2(pool1)
+    relu2 = self.relu(out2)
+    pool2 = self.pool(relu2)
+
+    gap = self.gap(pool2) # how strong was this feature across whole window
+    squeeze = gap.squeeze(-1) # remove leftover dimension size 1
+    x = self.fc(squeeze)
+    return x
