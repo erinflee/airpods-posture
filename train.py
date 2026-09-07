cedea37805e1687f9c4d15e669f785773bf06afe
erinlee316 <127688889+erinflee@users.noreply.github.com>
2026-09-07 12:36:50 -0700

    add train_one_epoch() to run one complete pass + update through CNN

--- train.py
+++ train.py
@@ -13,21 +13,33 @@ from model import PostureCNN
 
 
 def train_one_epoch(model, loader, optimizer, criterion, device):
+  total_loss = 0.0
+  model.train()
 
+  for X, y in loader:
+    X, y = X.to(device), y.to(device)
+    optimizer.zero_grad()
+    output = model(X)
+    loss = criterion(output, y)
+    loss.backward()
+    optimizer.step()
 
+    total_loss += loss.item() * len(y)
+
+  return total_loss / len(loader.dataset)
 
 
 def main():
 
   train, val, session = build_datasets(DATA_DIR)
 
-  model = PostureCNN()
-  train_dataloader = DataLoader(dataset=train, batch_size=BATCH_SIZE, shuffle=False)
+  device = "mps" if torch.backends.mps.is_available() else "cpu" # because i'm on mac and gpu not available
+  model = PostureCNN().to(device)
+
+  train_dataloader = DataLoader(dataset=train, batch_size=BATCH_SIZE, shuffle=True)
   val_dataloader = DataLoader(dataset=val, batch_size=BATCH_SIZE, shuffle=False)
 
-  optimizer = torch.optim.SGD(params=model.params, lr=LEARNING_RATE)
+  optimizer = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
   criterion = nn.CrossEntropyLoss() # common loss for classification problems
-
-  device = model.to("mps") # because i'm on mac and gpu not available
+ 
   train_one_epoch(model, train_dataloader, optimizer, criterion, device)
-
